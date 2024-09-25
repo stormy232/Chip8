ac31a64ead1bd0a2a340b8fc33aa76fded7375c3
stormy232 <stormy.dani@outlook.com>
2024-09-24 21:40:56 -0400

    merged main and chip8.cpp together added rudimentary rendering support

--- cpu.cpp
+++ cpu.cpp
@@ -3,6 +3,8 @@
 #include <random>
 #include <string.h>
 #include "chip8.hpp"
+#include <iostream>
+#include <fstream>
 
 
 	void CPU::OP_00E0(){
@@ -66,3 +68,73 @@
 
 			}
 		
+const unsigned int START_ADDRESS = 0x200;
+const unsigned int FONTSET_START_ADDRESS = 0x50;
+
+CPU::CPU() {
+
+  pc = START_ADDRESS;
+
+  for (unsigned int i = 0; i < 80; ++i) {
+
+    memory[FONTSET_START_ADDRESS + i] = fontset[i];
+  }
+}
+
+void CPU::LoadROM(char const *filename) {
+
+  std::ifstream file(filename, std::ios::binary | std::ios::ate);
+
+  if (file.is_open()) {
+
+    // get size of a file and create a buffer to hold the contents
+    std::streampos size = file.tellg();
+    char *buffer = new char[size];
+
+    // go to the beggining of the file and fill the buffer
+    file.seekg(0, std::ios::beg);
+    file.read(buffer, size);
+    file.close();
+
+    // load the ROM contents into the Chip8's memory, starting at the 0x200
+    for (long i = 0; i < size; ++i) {
+
+      memory[START_ADDRESS + i] = buffer[i];
+    }
+
+    std::cout << memory[514];
+    // Free the buffer
+    delete[] buffer;
+  }
+}
+
+void CPU::Cycle() {
+  opcode = (memory[pc] << 8) | memory[pc + 1];
+  pc += 2;
+  std::cout << pc << " ";
+  switch (opcode & 0xF000) {
+  case 0x0000:
+    OP_00E0();
+    std::cout << "00E0" << '\n';
+    break;
+  case 0x1000:
+    OneNNN();
+    std::cout << "1NNN" << '\n';
+    break;
+  case 0x6000:
+    SIXXNN();
+    std::cout << "6XNN" << '\n';
+    break;
+  case 0x7000:
+    SevenXNN();
+    std::cout << "7XNN" << '\n';
+    break;
+  case 0xA000:
+    ANNN();
+    std::cout << "ANNN" << '\n';
+    break;
+  case 0xD000:
+    DXYN();
+    std::cout << "DXYN" << '\n';
+  }
+}
--- makefile
+++ makefile
@@ -1,4 +1,4 @@
 main:chip8.cpp
 	g++ -c cpu.cpp
-	g++ -c chip8.cpp
-	g++ cpu.o chip8.o -o main `sdl2-config --cflags --libs`
+	g++ -c main.cpp
+	g++ cpu.o main.o -o main `sdl2-config --cflags --libs`
